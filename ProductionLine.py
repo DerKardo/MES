fa1feaf92a834443d49ff2b940d6eff6c1efd32f
Benjamin <benjamin.wiendl@wambo.com>
2024-06-11 12:55:59 +0200

    Added nameReturn

--- ProductionLine.py
+++ ProductionLine.py
@@ -1,11 +1,11 @@
 class ProductionLine: 
+    __name = ""
     def __init__(self, name): 
-
+        self.__name = name
     def add_order(self, order): 
-
+        pass
     def get_production_line_name(self): 
+        return self.__name
 
     def get_orders(self): 
-        
-
- 
+        pass
