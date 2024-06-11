366d81fae71a8619f9d7ff36d40ec4308e12c600
Benjamin <benjamin.wiendl@wambo.com>
2024-06-11 12:37:11 +0200

    Fixed Errors

--- ProductionOrder.py
+++ ProductionOrder.py
@@ -1,13 +1,13 @@
 class ProductionOrder: 
     def __init__(self, order_number, product_name, quantity): 
-
+        pass
     def get_order_number(self): 
-
+        pass
     def start(self): 
-
+        pass
     def finish(self): 
-
+        pass
     def produce(self, units): 
-
+        pass
     def get_production_progress(self): 
-        
+        pass
