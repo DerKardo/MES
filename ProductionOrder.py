b824e94ee6843c9f7b7db0c6fdc1e30d19c24a5f
Benjamin <benjamin.wiendl@wambo.com>
2024-06-11 12:53:03 +0200

    Finished

--- ProductionOrder.py
+++ ProductionOrder.py
@@ -1,13 +1,27 @@
 class ProductionOrder: 
-    def __init__(self, order_number, product_name, quantity): 
-        pass
+    __order_number = 0
+    __product_name = ""
+    __quantity = 0
+    __status = "created"
+
+    def __init__(self, order_number:int, product_name:str, quantity:int):
+        self.__order_number = order_number
+        self.__product_name = product_name
+        self.__quantity = quantity
+
     def get_order_number(self): 
-        pass
+        return self.__order_number
+    
     def start(self): 
+        self.__status = "started"
         pass
-    def finish(self): 
+    def finish(self):
+        self.__status = "finished"
         pass
     def produce(self, units): 
+        self.start
+        self.__quantity += units
+        self.finish
         pass
     def get_production_progress(self): 
-        pass
+        return self.__status
