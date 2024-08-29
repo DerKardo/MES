136b7352a5018b42c824a7afa2a375038b4b7ce5
Benjamin Wiendl <sr46701bw@live.com>
2024-08-29 08:36:51 +0200

    some MES stuff lol

--- MES.py
+++ MES.py
@@ -1,14 +1,26 @@
+from ProductionLine import ProductionLine
+
+
 class MES: 
     def __init__(self): 
+        self.productionLine = []
+        pass
 
     def add_production_line(self, name): 
+        self.productionLine.append(ProductionLine(name))
+        pass
 
     def create_production_order(self, production_line_name, order_number:int),  
-
+        self.get_production_line(production_line_name).add_order(order_number)
+        pass
     def produce_units(self, production_line_name, order_number, units): 
-
-    def get_production_lines(self): 
         
-    def get_production_line(self, name): 
+        pass
 
- 
+    def get_production_lines(self):
+        self.productionLine.
+        pass 
+        
+    def get_production_line(self, name)->ProductionLine:
+        return "hi"
+        pass 
