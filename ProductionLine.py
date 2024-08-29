d4fc017c918021ea25bf758b3b0ba685e1cace23
Benjamin Wiendl <sr46701bw@live.com>
2024-08-29 09:02:34 +0200

    Finished ProductionLine

--- ProductionLine.py
+++ ProductionLine.py
@@ -1,11 +1,16 @@
+import ProductionOrder
+
+
 class ProductionLine: 
     def __init__(self, name): 
+        self.name = name
+        self.orders = []
 
-    def add_order(self, order): 
+    def add_order(self, order:ProductionOrder): 
+        self.orders.append(order)
 
     def get_production_line_name(self): 
+        return(self.name)
 
     def get_orders(self): 
-        
-
- 
+        return self.orders
